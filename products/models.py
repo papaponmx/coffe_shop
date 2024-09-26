46d96ed75540793e69105e463c089ca088a9d80e
Jaime Rios <jaime.rios@hey.com>
2024-09-26 11:53:27 -0600

    Add products app to Django project

---
+++ products/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+# Create your models here.
+class Product(models.Model):
+    name = models.TextField(max_length=200, verbose_name="Nombre")
+    description = models.TextField(max_length=300, verbose_name="Descripcion")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Precio")
+    available = models.BooleanField(default=True)
+    photo = models.ImageField(
+        upload_to="products", null=True, blank=True, verbose_name="Imagen"
+    )
+
+    def __str__(self):
+        return self.name
