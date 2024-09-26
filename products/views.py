b4a82a0c7b09b7bfd96454d441cdf617ac3d6e56
Jaime Rios <jaime.rios@hey.com>
2024-09-26 13:54:53 -0600

    Refactor views and template for adding products
    - Update the views.py file in the products app to use the reverse_lazy function from django.urls instead of django.shortcuts for the success_url.
    - Add a form_valid method to the views.py file to save the form data.
    - Modify the add_product.html template to include a form for adding products.

--- products/views.py
+++ products/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
-from products.forms import ProductForm
+from .forms import ProductForm
 
 
 # Create your views here.
 class ProductFromView(generic.FormView):
     template_name = "products/add_product.html"
     form_class = ProductForm
+    success_url = reverse_lazy("add_product")
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
