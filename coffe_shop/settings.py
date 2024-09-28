6a09c66a904f133c63903c78abe5665e28e00a9c
Jaime Rios <jaime.rios@hey.com>
2024-09-27 18:59:03 -0600

    Refactor settings.py and urls.py to add 'crispy_forms' and 'crispy_tailwind' apps

--- coffe_shop/settings.py
+++ coffe_shop/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
+    "crispy_tailwind",
     "products",
     "users",
 ]
@@ -131,3 +133,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
+
+CRISPY_TEMPLATE_PACK = "tailwind"
+
+LOGIN_REDIRECT_URL = "product_list"
