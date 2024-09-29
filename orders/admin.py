f9466996757d2ced5615ed9892bc7949ada5a5f6
Jaime Rios <jaime.rios@hey.com>
2024-09-28 22:26:15 -0600

    Refactor admin.py to add OrderProductInlineAdmin

--- orders/admin.py
+++ orders/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 
-from orders.models import Order
+from orders.models import Order, OrderProuct
+
+
+class OrderProductInlineAdmin(admin.TabularInline):
+    model = OrderProuct
+    extra = 0
 
 
 class OrderAdmin(admin.ModelAdmin):
     model = Order
+    inlines = [OrderProductInlineAdmin]
 
 
 admin.site.register(Order, OrderAdmin)
