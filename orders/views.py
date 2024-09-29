f592538945b23f2c08173e96c3d67e0f553372d8
Jaime Rios <jaime.rios@hey.com>
2024-09-28 23:36:25 -0600

    Refactor views.py to add CreateOrderProductView

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
-from django.views.generic import DetailView
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from orders.forms import OrderProductForm
 from orders.models import Order
 
 
@@ -12,3 +14,18 @@ class MyOrderView(LoginRequiredMixin, DetailView):
 
     def get_object(self, querySet=None):
         return Order.objects.filter(is_active=True, user=self.request.user).first()
+
+
+class CreateOrderProductView(LoginRequiredMixin, CreateView):
+    template_name = "orders/create_order_product.html"
+    form_class = OrderProductForm
+    success_url = reverse_lazy("my_order")
+
+    def form_valid(self, form):
+        order, _ = Order.objects.get_or_create(is_active=True, user=self.request.user)[
+            0
+        ]
+        form.instance.order = order
+        form.instance.quantity = 1
+        form.save()
+        return super().form_valid(form)
